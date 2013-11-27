2e84f2985dca2998f03d4b158f52ad2cc65d1747
Andrew Wong <andrew12345@live.com>
2013-11-27 15:38:05 +1100

    Update facebook_autopoke.py

--- facebook_autopoke.py
+++ facebook_autopoke.py
@@ -2,9 +2,9 @@ import mechanize
 import time
 import os
 import sys
+USERNAME = sys.argv[0]
+PASSWORD = sys.argv[1]
 
-arg = sys.argv
-exp = 60
 browser = mechanize.Browser()
 browser.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0')]
 browser.set_handle_robots(False)
@@ -12,8 +12,8 @@ browser.set_handle_robots(False)
 browser.open("http://m.facebook.com/pokes")
 browser._factory.is_html = True
 browser.select_form(nr=0)
-browser.form['email'] = arg[0]
-browser.form['pass'] = arg[1]
+browser.form['email'] = USERNAME
+browser.form['pass'] = PASSWORD
 browser.submit()
 browser._factory.is_html = True
 while True:
@@ -26,11 +26,6 @@ while True:
 			if result:
 				browser.follow_link(text_regex="Poke back",nr=0)
                                 print("Poke!")
-                                if exp > 31:
-                                        exp -= 30
-                if exp < 180:
-                        exp += 1
-                time.sleep(exp)
 	except:
 		print "There was some sort of error :("
                 break
